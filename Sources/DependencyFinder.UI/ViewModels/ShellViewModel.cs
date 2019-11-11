5770d6c8bd7b649b645df9a936c1a8bd00633d83
dnf <dominik.jeske@hotmail.com>
2019-11-11 21:47:58 +0100

    Add WPF UI

---
+++ Sources/DependencyFinder.UI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Caliburn.Micro;
+
+namespace DependencyFinder.UI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        public ShellViewModel()
+        {
+            SolutionsRoot = @"E:\Projects\Dependency\DependencyFinder\Test";
+        }
+
+         public string SolutionsRoot { get; set; }
+
+    }
+}
